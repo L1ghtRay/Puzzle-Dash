3f4f882949344a8273b17d95c7c527057063cf5c
Kosen <polarist.light.ray@gmail.com>
2024-10-21 11:55:04 +0530

    Test Commit 6

--- src/Level/LevelSelector.java
+++ src/Level/LevelSelector.java
@@ -3,5 +3,5 @@ package Level;
 public class LevelSelector {
     Level l1 = new Level(101,"Level 1");
     Level l2 = new Level(102,"Level 2");
-    Level l3 = new Level(103,"Level Three");
+    Level l3 = new Level(103,"Level 3");
 }
